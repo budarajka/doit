c350131dd08031b08e5f15b49d38eec8e37d578b
budarajka <budarajka1@gmail.com>
2015-10-14 00:02:01 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Doit::Application.config.secret_token = 'dbdec2672b1bd25043c6dc2c59156713844c76b7da3a20f31d8cddb3f878c1c3d677dc57b41158d73a2c0c09c80ed52d925548bb032b069f2c8d8c029867e038'
